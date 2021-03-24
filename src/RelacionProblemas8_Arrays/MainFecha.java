abda2e76a3ce3dba86c665e36f4196d2d1a3ca2d
JavierCamposCuesta <javiercamposcuesta@gmail.com>
2021-03-24 10:05:47 +0100

    Actualizo 24/03/2021

--- src/RelacionProblemas8_Arrays/MainFecha.java
+++ src/RelacionProblemas8_Arrays/MainFecha.java
@@ -5,7 +5,7 @@ public class MainFecha {
 	public static void main(String[] args) {
 Fecha fecha= new Fecha();
 try {
-	System.out.println(fecha.comprobarFechaCorrecta(30, 2, 212));
+	System.out.println(fecha.comprobarFechaCorrecta(32, 12, 212));
 } catch (Exception e) {
 	
 	System.out.println(e.getMessage());
